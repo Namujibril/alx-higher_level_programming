71e63805b9d4498f55eeb6b86f66332c756f86a1
Namujibril <Namujibril@gmail.com>
2023-02-05 05:28:14 -0800

    Retest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,47 @@
 #!/usr/bin/python3
+"""
+This module is composed by a function that adds two numbers
+"""
+
+
 def add_integer(a, b=98):
-    """function that add 2 numbers"""
+    """ Function that adds two integer and/or float numbers
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        The addition of the two given numbers
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+    """
 
-    if not a or (type(a) != int and type(a) != float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)  #!/usr/bin/python3
 
-    return(int(a) + int(b))
+
+# This module is composed by a function that adds two numbers
+
+
+def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        The addition of the two given numbers
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
